909f1f92f75830ba983781f0e33c036fcdb68783
Jérémy Albrecht <me@jalbrecht.fr>
2026-06-25 12:01:31 +0200

    linting

--- custom_components/reseda/sensor.py
+++ custom_components/reseda/sensor.py
@@ -58,7 +58,11 @@ SENSORS: tuple[ResedaSensorDescription, ...] = (
         key="address",
         translation_key="address",
         entity_category=EntityCategory.DIAGNOSTIC,
-        value_fn=lambda d: f"{d.address.line}, {d.address.code_postal} {d.address.commune}" if d.address else None,
+        value_fn=lambda d: (
+            f"{d.address.line}, {d.address.code_postal} {d.address.commune}"
+            if d.address
+            else None
+        ),
     ),
 )
 
